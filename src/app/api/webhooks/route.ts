86249503ab05f9af9b07452db38539e632188d1c
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-09-28 01:37:50 +0530

    data sync using webhook

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,59 +1,83 @@
-import { Webhook } from "svix";
+import { Webhook, WebhookRequiredHeaders } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/db";
+import { IncomingHttpHeaders } from "http";
+import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+const webhookSecret = process.env.WEBHOOK_SECRET || "";
+
+async function handler(req: Request) {
+  console.log("inside webhook");
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
+    return NextResponse.json(
+      {
+        error:
+          "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
+      },
+      { status: 500 },
     );
   }
 
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
-
-  // Get the body
   const payload = await req.json();
-  const body = JSON.stringify(payload);
+  const headersList = headers();
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
-
-  let evt: WebhookEvent;
-
-  // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders,
+    ) as Event;
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
+    console.error((err as Error).message);
+    return NextResponse.json(
+      { error: "Webhook verification failed" },
+      { status: 400 },
+    );
   }
 
-  // Do something with the payload
-  // For this guide, you simply log the payload to the console
-  const { id } = evt.data;
-  const eventType = evt.type;
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
+  const eventType: EventType = evt.type;
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { id, ...attributes } = evt.data;
 
-  return new Response("", { status: 200 });
+    try {
+      await prisma.user.upsert({
+        where: { externalId: id as string },
+        create: {
+          externalId: id as string,
+          attributes,
+        },
+        update: { attributes },
+      });
+    } catch (error) {
+      console.error("Database error:", error);
+      return NextResponse.json({ error: "Database error" }, { status: 500 });
+    }
+  }
+
+  // Return a success response
+  return NextResponse.json(
+    { message: "Webhook processed successfully" },
+    { status: 200 },
+  );
 }
+
+type EventType = "user.created" | "user.updated" | "*";
+
+type Event = {
+  data: Record<string, string | number>;
+  object: "event";
+  type: EventType;
+};
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
